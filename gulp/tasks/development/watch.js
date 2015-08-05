1cd3aa0ec77e7bce28defc6ab3cc25f7f43ab3c6
Lincoln Brito <lincoln.sbrito@gmail.com>
2015-08-05 14:28:25 -0300

    complete watch task

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
+var config = require('../../config').watch;
 
 /* starts browsersync task then watch files for changes */
 gulp.task('watch', ['browsersync'], function(){
-
+    gulp.watch(config.jekyll,  ['jekyll-rebuild']);
+    gulp.watch(config.sass,    ['sass']);
+    //gulp.watch(config.scripts, ['sass']);
+    gulp.watch(config.images,  ['images']);
+    //gulp.watch(config.svg,     ['copy:fonts']);
+    //gulp.watch(config.sprites, ['sprites']);
 });
