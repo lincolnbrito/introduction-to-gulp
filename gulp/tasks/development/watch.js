6a70cf111b6f454574a895137dd5ca1978982164
Lincoln Brito <lincoln.sbrito@gmail.com>
2015-08-05 14:35:06 -0300

    changing watch task

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -5,8 +5,9 @@ var config = require('../../config').watch;
 gulp.task('watch', ['browsersync'], function(){
     gulp.watch(config.jekyll,  ['jekyll-rebuild']);
     gulp.watch(config.sass,    ['sass']);
-    //gulp.watch(config.scripts, ['sass']);
     gulp.watch(config.images,  ['images']);
+    //gulp.watch(config.sass,    ['sass', 'scsslint']);
+    //gulp.watch(config.scripts, ['scripts', 'jshint']);
     //gulp.watch(config.svg,     ['copy:fonts']);
     //gulp.watch(config.sprites, ['sprites']);
 });
