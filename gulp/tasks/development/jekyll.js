05add1f16160b202730bc060b0f8eaf0a7303ffc
Lincoln Brito <lincoln.sbrito@gmail.com>
2015-08-05 09:34:07 -0300

    changing jekyll

--- gulp/tasks/development/jekyll.js
+++ gulp/tasks/development/jekyll.js
@@ -1,6 +1,6 @@
 var gulp        = require('gulp');
 var cp          = require('child_process');
-var browsersync = require('browser-sync').create();
+var browsersync = require('browser-sync');
 var config      = require('../../config').jekyll.development;
 
 /**
