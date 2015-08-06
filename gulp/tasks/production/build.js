d539cde1c9a1ffb50352a7dbecd93c6496258a52
Lincoln Brito <lincoln.sbrito@gmail.com>
2015-08-06 08:48:15 -0300

    add build and jekyll task

---
+++ gulp/tasks/production/build.js
@@ -0,0 +1,25 @@
+var gulp         = require('gulp');
+var reunSequence = require('run-sequence');
+
+// Run all taks needed for a build in define order
+gulp.task('build:production', function(callback){
+    reunSequence('delete', 'jekyll:production',
+        [
+            'sass',
+            'scripts',
+            'images',
+            'copy:fonts'
+        ],
+        'base64',
+        [
+            'optmize:css',
+            'optmize:js',
+            'optmize:images',
+            'optmize:html',
+            'optmize:fonts:production',
+        ],
+        'revision',
+        'rev:collect'
+        callback
+    );
+})
---
+++ gulp/tasks/production/jekyll.js
@@ -0,0 +1,46 @@
+var gulp        = require('gulp');
+var cp          = require('chil_process');
+var browsersync = require('browser-sync');
+var config      = require('../../config').jekyll.production;
+
+//build the Jekyll site
+gulp.task('jekyll:production', function(done){
+    browsersync.notify('Compiling Jekyll');
+
+    /**
+    * Resolving problem when run child_process on Windows
+    * @see http://matthew-jackson.com/notes/development/node-child_process-enoent-error-windows/
+    */
+    if(process.platform === "win32") {
+        return cp.spawn('cmd', 
+           [
+                '/c',        
+                'jekyll', 
+                'build', 
+                '-q', 
+                '-s'+config.src, 
+                '-d'+config.dest, 
+                '-c'+config.src+'/'+config.config
+            ], 
+            { 
+                stdio: 'inherit' 
+            }
+        )
+        .on('close', done);
+    }else{
+        return cp.spawn(
+            'bundle', 
+            [
+                'exec', 
+                'jekyll', 
+                'build', 
+                '-q', 
+                '--source=' + config.src, 
+                '--destination=' + config.dest, 
+                '--config=' + config.config
+            ], 
+            { stdio: 'inherit' }
+        )
+        .on('close', done);
+    }
+});
